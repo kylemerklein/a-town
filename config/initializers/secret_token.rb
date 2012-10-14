c8b13b98274ca996b9bcdf9dbed20321d3b878de
Kyle Merklein <kyle.merklein@gmail.com>
2012-10-13 21:54:18 -0500

    creating the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ATown::Application.config.secret_token = 'f2e84a34b6988dcf78409b0ea8db7081aa17e4a6307437d95e7dbf80a7c5b8b95e31f6d952eec57dd5877ba86cb1d2c6ec1090be8b5406bc84bcd37173e3ed5f'
